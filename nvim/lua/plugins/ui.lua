cb1bc07207275f0c5dcaeb264a4a21b80d7eac69
Jon Linkens <jonlinkens@outlook.com>
2024-04-04 22:38:16 +0100

    Make neovim bg transparent

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,7 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {
+    transparent_background = true,
+  } },
   {
     "LazyVim/LazyVim",
     opts = {
