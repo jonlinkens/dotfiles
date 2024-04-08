ae524cc4b39a0a850e3b33f47b20646776bb6c7c
Jon Linkens <linkensjon@gmail.com>
2024-04-08 11:55:17 +0100

    show dotfiles etc. on neotree

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -11,4 +11,22 @@ return {
   {
     "dstein64/nvim-scrollview",
   },
+  {
+    "neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          --visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".github",
+            ".gitignore",
+            "package-lock.json",
+          },
+          never_show = { ".git" },
+        },
+      },
+    },
+  },
 }
