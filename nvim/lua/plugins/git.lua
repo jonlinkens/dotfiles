a2324ebd6380f1e9bddad5467c0d2931186984a1
Jon Linkens <linkensjon@gmail.com>
2024-05-20 12:03:27 +0100

    add gitsigns

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = true,
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 500,
+        relative_time = true,
+      },
+    },
+  },
+}
