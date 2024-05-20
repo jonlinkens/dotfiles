0e3bdbc7732769de9d16ef39449262f9d46b8ae8
Jon Linkens <linkensjon@gmail.com>
2024-05-20 12:04:56 +0100

    disable format on save autocmd
    its super buggy

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,19 +1,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
-local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup("removeImports"),
-  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
-  callback = function()
-    vim.lsp.buf.code_action({
-      apply = true,
-      context = {
-        only = { "source.removeUnusedImports.ts" },
-        diagnostics = {},
-      },
-    })
-  end,
-})
+-- local function augroup(name)
+--   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+-- end
+--
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   group = augroup("removeImports"),
+--   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+--   callback = function()
+--     vim.lsp.buf.code_action({
+--       apply = true,
+--       context = {
+--         only = { "source.removeUnusedImports.ts" },
+--         diagnostics = {},
+--       },
+--     })
+--   end,
+-- })
