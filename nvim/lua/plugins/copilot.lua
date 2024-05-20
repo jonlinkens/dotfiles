a211da73212ce71d3422ac96a4db9f917b53a696
Jon Linkens <linkensjon@gmail.com>
2024-05-20 12:04:34 +0100

    adjust copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,23 +1,25 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    panel = {
-      auto_refresh = false,
-      keymap = {
-        accept = "<CR>",
-        jump_prev = "[[",
-        jump_next = "]]",
-        refresh = "gr",
-        open = "<M-CR>",
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      panel = {
+        auto_refresh = false,
+        keymap = {
+          accept = "<CR>",
+          jump_prev = "[[",
+          jump_next = "]]",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
       },
-    },
-    suggestion = {
-      accept = false,
-      auto_trigger = true,
-      keymap = {
-        prev = "<M-[>",
-        next = "<M-]>",
-        dismiss = "<C-]>",
+      suggestion = {
+        accept = false,
+        auto_trigger = true,
+        keymap = {
+          prev = "<M-[>",
+          next = "<M-]>",
+          dismiss = "<C-]>",
+        },
       },
     },
   },
