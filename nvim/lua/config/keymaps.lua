be9a2a6db765f14b4d5f11af2018e208d7a86071
Jon Linkens <linkensjon@gmail.com>
2024-06-01 13:50:50 +0100

    add more keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,3 +13,30 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 vim.api.nvim_set_keymap("n", "<leader>cpd", ":Copilot disable<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>cpe", ":Copilot enable<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>cg",
+  ":Mdpreview<CR>",
+  { noremap = true, silent = true, desc = "Open markdown preview buffer" }
+)
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>gv",
+  ":OpenInGHFile <CR>",
+  { silent = true, noremap = true, desc = "Open in GitHub" }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "<Leader>gv",
+  ":OpenInGHFileLines <CR>",
+  { silent = true, noremap = true, desc = "Open lines in GitHub" }
+)
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>t",
+  ":GlobalNote <CR>",
+  { silent = true, noremap = true, desc = "Open global note" }
+)
