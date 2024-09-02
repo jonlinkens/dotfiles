025327e2b8d6eb4077a42cbb4df3ec78aa8f403f
Jon Linkens <linkensjon@gmail.com>
2024-09-02 09:57:58 +0100

    add conform comment

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,9 @@ return {
   cmd = { "ConformInfo" },
   opts = {
     formatters_by_ft = {
+      -- interchange biome with prettier or else it doesnt work...
+      -- Make sure its installed with mason and conform
+
       javascript = { { "prettier" } },
       typescript = { { "prettier" } },
     },
