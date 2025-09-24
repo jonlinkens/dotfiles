e86045d98ca47125a4ce5d0267102143536d8073
Jon Linkens <linkensjon@gmail.com>
2025-09-24 12:27:31 +0100

    use typescript-tools.nvim
    been using this forever but forgot to commit

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,19 +2,21 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-
       inlay_hints = { enabled = false },
       servers = {
         astro = {},
+        tsserver = {
+          enabled = false,
+        },
+        ts_ls = {
+          enabled = false,
+        },
         vtsls = {
-          settings = {
-            vtsls = { autoUseWorkspaceTsdk = true },
-          },
+          enabled = false,
         },
       },
     },
   },
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -38,6 +40,22 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
+      },
+    },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      settings = {
+        code_lens = "off",
+        separate_diagnostic_server = true,
+        publish_diagnostic_on = "insert_leave",
+        tsserver_path = nil,
+        tsserver_max_memory = 10000,
+        tsserver_format_options = {},
+        tsserver_locale = "en",
+        disable_member_code_lens = true,
       },
     },
   },
