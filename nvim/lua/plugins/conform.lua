95d3f37c9b8c7eff39def2fb9839166ae9eb08e1
Jon Linkens <linkensjon@gmail.com>
2026-03-02 17:01:26 +0000

    nvim: add oxfmt

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,16 +6,19 @@ return {
     notify_on_error = true,
     formatters = {
       biome = { require_cwd = true },
-      ["biome-check"] = { require_cwd = true },
+      ["biome-check"] = {
+        require_cwd = true,
+      },
       prettier = { require_cwd = true },
+      oxfmt = { require_cwd = true },
     },
     formatters_by_ft = {
       lua = { "stylua" },
-      ["javascript"] = { "biome-check", "prettier", stop_after_first = true },
-      ["javascriptreact"] = { "biome-check", "prettier", stop_after_first = true },
-      ["typescript"] = { "biome-check", "prettier", stop_after_first = true },
-      ["typescriptreact"] = { "biome-check", "prettier", stop_after_first = true },
-      ["json"] = { "biome-check", "prettier", stop_after_first = true },
+      ["javascript"] = { "biome-check", "oxfmt", "prettier", stop_after_first = true },
+      ["javascriptreact"] = { "biome-check", "oxfmt", "prettier", stop_after_first = true },
+      ["typescript"] = { "biome-check", "oxfmt", "prettier", stop_after_first = true },
+      ["typescriptreact"] = { "biome-check", "oxfmt", "prettier", stop_after_first = true },
+      ["json"] = { "biome-check", "oxfmt", "prettier", stop_after_first = true },
     },
   },
 }
