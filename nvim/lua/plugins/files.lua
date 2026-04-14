95930bd652ce9848f93dc3a5cc2c351198cf6663
Jon Linkens <linkensjon@gmail.com>
2026-04-14 11:20:07 +0100

    nvim: fzf-lua -> fff.nvim

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -1,4 +1,79 @@
+local find_files_opts = {
+  win = {
+    input = {
+      keys = {
+        ["<C-g>"] = {
+          "toggle_live",
+          mode = { "n", "i" },
+        },
+        ["<S-Tab>"] = {
+          "go_to_live_grep",
+          mode = { "n", "i" },
+          nowait = true,
+        },
+      },
+    },
+  },
+}
+
+local live_grep_opts = {
+  win = {
+    input = {
+      keys = {
+        ["<C-g>"] = {
+          "toggle_live",
+          mode = { "n", "i" },
+        },
+        ["<S-Tab>"] = {
+          "go_to_find_files",
+          mode = { "n", "i" },
+          nowait = true,
+        },
+      },
+    },
+  },
+}
+
+local fuzzy_grep_opts = vim.deepcopy(live_grep_opts)
+fuzzy_grep_opts.grep_mode = { "fuzzy", "plain", "regex" }
+
 return {
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>ff", false },
+    },
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.input = opts.input or {}
+      opts.picker = opts.picker or {}
+      opts.picker.actions = opts.picker.actions or {}
+
+      opts.picker.actions.go_to_live_grep = function(picker)
+        picker:close()
+        live_grep_opts.search = picker.input.filter.search
+        require("fff-snacks").live_grep(live_grep_opts)
+      end
+
+      opts.picker.actions.go_to_find_files = function(picker)
+        picker:close()
+        find_files_opts.search = picker.input.filter.search
+        require("fff-snacks").find_files(find_files_opts)
+      end
+    end,
+  },
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader>ff", false },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>ff", false },
+    },
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     enabled = "false",
@@ -35,26 +110,48 @@ return {
     },
   },
   {
-    "ibhagwan/fzf-lua",
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+      require("fff.download").download_or_build_binary()
+    end,
+    lazy = false,
     opts = {
-      oldfiles = {
-        include_current_session = true,
-      },
-      previewers = {
-        builtin = {
-          syntax_limit_b = 1024 * 100, -- 100KB
-        },
-      },
-      grep = {
-        rg_glob = true, -- enable glob parsing
-        glob_flag = "--iglob", -- case insensitive globs
-      },
+      lazy_sync = true,
     },
+  },
+  {
+    "madmaxieee/fff-snacks.nvim",
+    lazy = false,
     keys = {
-      { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (cwd)" },
-      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (Root files)" },
-      { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
-      { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+      {
+        "<leader>ff",
+        function()
+          require("fff-snacks").find_files(find_files_opts)
+        end,
+        desc = "FFF find files",
+      },
+      {
+        "<leader>fw",
+        function()
+          require("fff-snacks").live_grep(live_grep_opts)
+        end,
+        desc = "FFF live grep",
+      },
+      {
+        mode = "v",
+        "<leader>fw",
+        function()
+          require("fff-snacks").grep_word(live_grep_opts)
+        end,
+        desc = "FFF grep word",
+      },
+      {
+        "<leader>fz",
+        function()
+          require("fff-snacks").live_grep(fuzzy_grep_opts)
+        end,
+        desc = "FFF live grep (fuzzy)",
+      },
     },
   },
 }
